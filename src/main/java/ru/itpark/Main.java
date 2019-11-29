72ece3c3976df799efdf8c4108d42d2e51cbe8cb
bertRC <sithalbert@ya.ru>
2019-11-29 18:27:20 +0300

    input from console added

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,7 +1,14 @@
 package ru.itpark;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your name please: ");
+        String name = reader.readLine();
+        System.out.println(name);
     }
 }
