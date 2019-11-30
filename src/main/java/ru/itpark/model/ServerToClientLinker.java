651bb5262f864d9333b7aeca6f3c59310c4a06d8
bertRC <sithalbert@ya.ru>
2019-11-30 12:10:46 +0300

    send and readline methods added

--- src/main/java/ru/itpark/model/ServerToClientLinker.java
+++ src/main/java/ru/itpark/model/ServerToClientLinker.java
@@ -11,4 +11,18 @@ public class ServerToClientLinker {
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
+
+    public void send(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readline() throws IOException {
+        return reader.readLine();
+    }
 }
