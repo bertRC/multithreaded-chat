241189cd30763bec39925b65b1ab1849280909bb
bertRC <sithalbert@ya.ru>
2019-11-30 12:19:04 +0300

    ChatClient class refactored again

--- src/main/java/ru/itpark/client/ChatClient.java
+++ src/main/java/ru/itpark/client/ChatClient.java
@@ -1,5 +1,7 @@
 package ru.itpark.client;
 
+import ru.itpark.model.ServerToClientLinker;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -8,23 +10,19 @@ public class ChatClient {
         try (
                 final BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
                 final Socket socket = new Socket("localhost", 9876);
-                final BufferedReader chatReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final BufferedWriter chatWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         ) {
+            final ServerToClientLinker linker = new ServerToClientLinker(socket);
+
             System.out.print("Enter your name please: ");
             final String name = consoleReader.readLine();
 
-            chatWriter.write(name + " has joined.");
-            chatWriter.newLine();
-            chatWriter.flush();
+            linker.send(name + " has joined.");
 
             new Thread(() -> {
                 String message;
                 try {
                     while ((message = consoleReader.readLine()) != null) {
-                        chatWriter.write(name + ": " + message);
-                        chatWriter.newLine();
-                        chatWriter.flush();
+                        linker.send(name + ": " + message);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -32,7 +30,7 @@ public class ChatClient {
             }).start();
 
             String line;
-            while ((line = chatReader.readLine()) != null) {
+            while ((line = linker.readLine()) != null) {
                 System.out.println(line);
             }
 
