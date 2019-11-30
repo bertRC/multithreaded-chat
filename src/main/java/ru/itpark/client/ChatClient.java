bf5c17ac5f01e6fe8ef140a712ad265ba973e73f
bertRC <sithalbert@ya.ru>
2019-11-30 11:47:41 +0300

    ChatClient class refactored

--- src/main/java/ru/itpark/client/ChatClient.java
+++ src/main/java/ru/itpark/client/ChatClient.java
@@ -8,6 +8,7 @@ public class ChatClient {
         try (
                 final BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
                 final Socket socket = new Socket("localhost", 9876);
+                final BufferedReader chatReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 final BufferedWriter chatWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         ) {
             System.out.print("Enter your name please: ");
@@ -17,18 +18,7 @@ public class ChatClient {
             chatWriter.newLine();
             chatWriter.flush();
 
-            final Thread chatReaderThread = new Thread(() -> {
-                String line;
-                try (final BufferedReader chatReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    while ((line = chatReader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-
-            final Thread chatWriterThread = new Thread(() -> {
+            new Thread(() -> {
                 String message;
                 try {
                     while ((message = consoleReader.readLine()) != null) {
@@ -39,15 +29,14 @@ public class ChatClient {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            });
+            }).start();
 
-            chatReaderThread.start();
-            chatWriterThread.start();
+            String line;
+            while ((line = chatReader.readLine()) != null) {
+                System.out.println(line);
+            }
 
-            chatReaderThread.join();
-            chatWriterThread.join();
-
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
