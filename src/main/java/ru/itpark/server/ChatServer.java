8520baccbe40b117e08eab74007a7d1da38e6908
bertRC <sithalbert@ya.ru>
2019-12-05 18:57:00 +0300

    fixes #2

--- src/main/java/ru/itpark/server/ChatServer.java
+++ src/main/java/ru/itpark/server/ChatServer.java
@@ -5,12 +5,12 @@ import ru.itpark.model.ServerToClientConnector;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ChatServer {
     public static void main(String[] args) {
-        final Set<ServerToClientConnector> connectors = new HashSet<>();
+        final BlockingQueue<ServerToClientConnector> connectors = new LinkedBlockingQueue<>();
         try (ServerSocket serverSocket = new ServerSocket(9876)) {
             while (true) {
                 final Socket socket = serverSocket.accept();
